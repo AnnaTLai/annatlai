9f90f9d81cd700f2e9ccfd5cb06d680e16caf95d
AnnaTLai <annateresa.lai@mail.utoronto.ca>
2024-10-20 15:17:43 -0400

    dev deploy file change

---
+++ Old/README.md
@@ -0,0 +1,12 @@
+### Hello this is Anna :D 👋
+
+<!--
+**AnnaTLai/annatlai** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+-->
+- 👯 I’m looking to collaborate on projects related to healthcare 👩‍⚕️
+- 📫 How to reach me: [LinkedIn](https://www.linkedin.com/in/annatlai/)
+- 😄 Pronouns: She/ Her
+- ⚡ Fun fact: Glass knife fish is a type of electric fish that can be kept in aquariums. 
+
---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://annatlai.github.io/annatlai/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true, // Generate sitemap (set to "false" to disable)
+  base:"annatlai",
+  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+});
