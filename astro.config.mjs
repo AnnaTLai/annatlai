9e396382182275c47ac6872560803a29cda63f0f
AnnaTLai <annateresa.lai@mail.utoronto.ca>
2024-10-20 15:51:02 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import lit from "@astrojs/lit";
 export default defineConfig({
   site: 'https://annatlai.github.io/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
-  base:"annatlai",
+  base:"",
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
